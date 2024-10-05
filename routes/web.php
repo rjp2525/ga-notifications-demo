143fc7bcad5d65d8360728ff78b6df06f13a43d9
rjp2525 <renophilibert@gmail.com>
2024-10-05 11:22:08 -0600

    Update routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AnnouncementController;
+use App\Http\Controllers\CreateAnnouncementController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
+use App\Models\Announcement;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -20,9 +23,20 @@ Route::get('/dashboard', DashboardController::class)
     ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/announcement/{announcement:slug}', AnnouncementController::class)
+        ->name('announcement.view');
+    Route::get('/new-announcement', [CreateAnnouncementController::class, 'create'])
+        ->name('announcement.create')
+        ->can('create', Announcement::class);
+    Route::post('/new-announcement', [CreateAnnouncementController::class, 'store'])
+        ->can('create', Announcement::class);
+
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
